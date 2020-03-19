59a698f9ed34131b8280d5511828429863086833
Minh Tran <minhtran.se@gmail.com>
2020-03-19 16:22:37 +0700

    Updates

---
+++ precache-manifest.87d5d47c7f789370be31a3e76fa860c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd9d02f5990c3894082bba2ed348020",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "6bc089881afac9414d39",
+    "url": "/dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00bfd53d595ab0cd1ffc",
+    "url": "/dashboard/static/js/2.5ca322fd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dashboard/static/js/2.5ca322fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc089881afac9414d39",
+    "url": "/dashboard/static/js/main.af258a27.chunk.js"
+  },
+  {
+    "revision": "9a34a5c47afc48018561",
+    "url": "/dashboard/static/js/runtime-main.5fa48c14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
