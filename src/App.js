47e381f128aa45510d68190bb7431a78b49e7265
Minh Tran <minhtran.se@gmail.com>
2020-03-20 08:33:21 +0700

    First try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dashboard App using React and Bootstrap!
+          Congratulation! You've successfully sync local repository to GitHub Pages repository. Enjoy!
         </p>
         <a
           className="App-link"
