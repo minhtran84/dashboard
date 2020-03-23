d6ddf621c15ccc0417915ba91d6a67471c9f988c
Minh Tran <minhtran.se@gmail.com>
2020-03-23 10:53:02 +0700

    Create DoughnutCard component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import './App.css';
 
 import Nagigation from './components/Navigation';
-import SimpleCart from './components/SimpleCard';
+import DoughnutCard from './components/DoughnutCard';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        
+
           <Nagigation />
-        
-      </div>
+
+          <div>{/* start Third Row  */}
+          <div class="container mb-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <DoughnutCard header="Mức độ hài lòng" />
+                </div>
+                <div class="col-md-6">
+                    {/* <ChartCard header="Hồ sơ trực tuyến" /> */}
+                </div>
+            </div>
+          </div>
+        </div>{/* end Third Row */}
+
+      </div>//end div app
     );
   }
 }
---
+++ src/components/DoughnutCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import '../styles/doughnutcard.css';
+
+class DoughnutCard extends React.Component {
+
+    render() {
+        return (
+            <div class="card" className="doughnutcard">
+                <div class="card-header">
+                    <h4 className="doughnutcard_header">{this.props.header}</h4>
+                </div>
+                <div class="card-body">
+                    <Doughnut 
+                        data={{
+                            labels: ["Hài lòng","Không hài lòng"],
+                            datasets: [{
+                                label: "Revenue",
+                                backgroundColor: ["#5FAD56","#E55934"],
+                                borderColor: ["rgb(39,42,61)","rgb(39,42,61)"],
+                                data: ["1007", "150"], //real data
+                            }],
+                            }
+                        }
+                        width={816}
+                        height={408}
+                        options={{
+                            maintainAspectRatio: true,
+                            legend: {
+                                display: true,
+                                reverse: false
+                            },
+                            title: {}
+                        }}
+                        className="doughnutcard_body"
+                    />
+
+                    <div>
+                        {/* <canvas height="408" width="816" className="chartcard_body"></canvas> */}
+
+            
+                        {/* <canvas data-bs-chart="{style="display: block; height: 204px; width: 408px;" width="306" height="153" ></canvas> */}
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DoughnutCard;
