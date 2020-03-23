4dd9ff37fefa1895faee03a551b38194520c0afd
Minh Tran <minhtran.se@gmail.com>
2020-03-23 17:03:36 +0700

    nearly finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Nagigation from './components/Navigation';
+import SimpleCard from './components/SimpleCard';
+import ProCard from './components/ProCard';
 import DoughnutCard from './components/DoughnutCard';
 import BarCard from './components/BarCard';
+import Footer from './components/Footer';
 
 class App extends React.Component {
 
@@ -13,18 +16,49 @@ class App extends React.Component {
 
           <Nagigation />
 
-          <div>{/* start Third Row  */}
-          <div class="container mb-3">
-            <div class="row">
-                <div class="col-md-6">
-                    <DoughnutCard header="Mức độ hài lòng" />
-                </div>
-                <div class="col-md-6">
-                    <BarCard header="Hồ sơ trực tuyến"/>
+          <div>{/* start First Row  */}
+            <div class="container mt-3 mb-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <SimpleCard header="Tổng số TTHC" number="1089" icon="faFileAlt"/>
+                    </div>
+                    <div class="col-md-4">
+                        <SimpleCard header="Sở ngành" number="17" icon="faBuilding"/>
+                    </div>
+                    <div class="col-md-4">
+                        <SimpleCard header="Quầy giao dịch" number="23" icon="faDesktop"/>
+                    </div>
                 </div>
             </div>
-          </div>
-        </div>{/* end Third Row */}
+          </div>{/* end First Row  */}
+
+          <div>{/* start Second Row  */}
+            <div class="container mb-3">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ProCard header="Tình hình xử lý HS" number="11725" cardType="tinhhinhxl" />
+                    </div>
+                    <div class="col-md-6">
+                        <ProCard header="HS trực tuyến" number="5734" cardType="hstructuyen" />
+                    </div>
+                </div>
+            </div>
+          </div>{/* end Second Row */}
+
+          <div>{/* start Third Row  */}
+            <div class="container mb-3">
+              <div class="row">
+                  <div class="col-md-6">
+                      <DoughnutCard header="Mức độ hài lòng" />
+                  </div>
+                  <div class="col-md-6">
+                      <BarCard header="Hồ sơ trực tuyến"/>
+                  </div>
+              </div>
+            </div>
+          </div>{/* end Third Row */}
+
+          <Footer />
 
       </div>//end div app
     );
--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -1,20 +1,44 @@
 import React from 'react';
+import "../styles/simplecard.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+//import which icons to be used 
+import { faFileAlt } from '@fortawesome/free-regular-svg-icons';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { faDesktop } from '@fortawesome/free-solid-svg-icons';
 
 class SimpleCard extends React.Component {
 
-    constructor(props) {
-        super(props);
-        
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
 
-    componentDidMount() {
-
-        
+    //conditional render
+    renderSwitch(param) {
+        switch(param) {
+            case 'faFileAlt':
+                return <FontAwesomeIcon icon={faFileAlt} className="simplecard_body_icon" />;
+            case 'faBuilding':
+                return <FontAwesomeIcon icon={faBuilding} className="simplecard_body_icon_sbn" />;
+            case 'faDesktop':
+                return <FontAwesomeIcon icon={faDesktop} className="simplecard_body_icon_quay" />;
+            default:
+                return null;
+        }
     }
 
     render() {
         return (
-            <h5>SimpleCard Component</h5>
+            <div class="card" className="simplecard">
+                <div class="card-header">
+                    <h4 className="simplecard_header">{this.props.header}</h4>
+                </div>
+                <div class="card-body">
+                    { this.renderSwitch(this.props.icon) }
+                    <h2>{this.props.number}</h2>
+                </div>
+            </div>
         );
     }
 }
